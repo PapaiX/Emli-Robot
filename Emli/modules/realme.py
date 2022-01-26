c222f60fcf7b19ac9bcbeb08dcaa3978ffff756f
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2022-01-26 16:50:10 +0530

    Update realme.py

--- Emli/modules/realme.py
+++ Emli/modules/realme.py
@@ -1,8 +1,11 @@
 import re
+from requests import get
 
 import rapidjson as json
 from bs4 import BeautifulSoup
 from Emli import pbot
+
+from Emli.utils.devices import GetDevice
 
 from hurry.filesize import size as sizee
 from pyrogram import Client, filters
@@ -68,48 +71,47 @@ async def realmeui(c: Client, update: Update):
 
 
 @pbot.on_message(filters.command("samspec"))
-async def samspecs(c: Client, update: Update):
-    if len(update.command) != 2:
-        message = (
-            "Please write your codename or model into it,\ni.e <code>/specs herolte</code> or <code>/specs sm-g610f</code>")
-        await c.send_message(
-            chat_id=update.chat.id,
-            text=message)
+async def specs(bot, update):
+    if not len(update.command) == 2:
+        message = "Please write your codename or model into it,\ni.e <code>/specs herolte</code> or <code>/specs sm-g610f</code>"
+        await bot.send_message(chat_id=update.chat.id, text=message)
         return
     device = update.command[1]
     data = GetDevice(device).get()
     if data:
-        name = data['name']
-        model = data['model']
-        device = name.lower().replace(' ', '-')
+        name = data["name"]
+        model = data["model"]
+        device = name.lower().replace(" ", "-")
     else:
         message = "coudn't find your device, chack device & try!"
-        await c.send_message(
-            chat_id=update.chat.id,
-            text=message)
+        await bot.send_message(chat_id=update.chat.id, text=message)
         return
-    sfw = get(f'https://sfirmware.com/samsung-{model.lower()}/')
+    sfw = get(f"https://sfirmware.com/samsung-{model.lower()}/")
     if sfw.status_code == 200:
-        page = BeautifulSoup(sfw.content, 'lxml')
-        message = '<b>Device:</b> Samsung {}\n'.format(name)
-        res = page.find_all('tr', {'class': 'mdata-group-val'})
+        page = BeautifulSoup(sfw.content, "lxml")
+        message = "<b>Device:</b> Samsung {}\n".format(name)
+        res = page.find_all("tr", {"class": "mdata-group-val"})
         res = res[2:]
         for info in res:
-            title = re.findall(r'<td>.*?</td>', str(info)
-                               )[0].strip().replace('td', 'b')
-            data = re.findall(r'<td>.*?</td>', str(info)
-                              )[-1].strip().replace('td', 'code')
+            title = re.findall(r"<td>.*?</td>", str(info))[0].strip().replace("td", "b")
+            data = (
+                re.findall(r"<td>.*?</td>", str(info))[-1].strip().replace("td", "code")
+            )
             message += "• {}: <code>{}</code>\n".format(title, data)
-
+        await bot.send_message(chat_id=update.chat.id, text=message)
     else:
-        message = "Device specs not found in bot database, make sure this is a Samsung device!"
-        await c.send_message(
-            chat_id=update.chat.id,
-            text=message)
-        return
+        giz = get(f"https://www.gizmochina.com/product/samsung-{device}/")
+        if giz.status_code == 404:
+            message = "device specs not found in bot databases!"
+            await bot.send_message(chat_id=update.chat.id, text=message)
+            return
+        page = BeautifulSoup(giz.content, "lxml")
+        message = "<b>Device:</b> Samsung {}\n".format(name)
+        for info in page.find_all("div", {"class": "aps-feature-info"}):
+            title = info.find("strong").text
+            data = info.find("span").text
+            message += "• {}: <code>{}</code>\n".format(title, data)
+        await bot.send_message(chat_id=update.chat.id, text=message)
 
-    await c.send_message(
-        chat_id=update.chat.id,
-        text=message)
 
 _mod_name_ = "realme"
